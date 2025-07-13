12a87658028062accd4217862ffcbab56202000d
devadh21 <devadh21@gmail.com>
2025-07-13 15:39:28 +0000

    update two

---
+++ components/Hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center">Welcome to Devadh.com</h1>
+      <div>
+        <Image
+          src="/hero-image.png"
+          alt="Hero Image"
+          width={1020}
+          height={400}
+        />
+      </div>
+      <div className="text-center mt-8">
+        <Link href={"https://portfolio.devadh.com"} target="_blank"  className="bg-purple-900 text-white px-4 py-2 rounded x hover:bg-purple-700 transition-colors">
+          My Portfolio
+        </Link>
+      </div>
+    </div>
+  );
+}
