6af415c55bf56196ac134c4ebfac98758a4cf7b1
devadh21 <devadh21@gmail.com>
2025-07-16 11:25:58 +0000

    modify link my portfolio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
         />
       </div>
       <div className="text-center mt-8">
-        <Link href={"https://portfolio.devadh.info"} target="_blank"  className="bg-purple-900 text-white px-4 py-2 rounded  hover:bg-purple-700 transition-colors">
+        <Link href={"https://portfolio.devadh.com"} target="_blank"  className="bg-purple-900 text-white px-4 py-2 rounded  hover:bg-purple-700 transition-colors">
           My Portfolio
         </Link>
       </div>
