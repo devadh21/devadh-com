1be0b8cc2ed44d343d6a5f05577ed87997dc1d9f
devadh21 <devadh21@gmail.com>
2025-08-05 16:43:29 +0000

    add pdf doc

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,6 +18,9 @@ export default function Hero() {
         <Link href={"https://portfolio.devadh.com"} target="_blank"  className="bg-purple-900 text-white px-4 py-2 rounded  hover:bg-purple-700 transition-colors">
           My Portfolio
         </Link>
+        <a href="/docs/doc.pdf" download>
+  Download PDF
+</a>
       </div>
     </div>
   );
