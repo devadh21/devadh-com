ce76a62368268db85787535f87aa79c0c8c7e4be
devadh21 <devadh21@gmail.com>
2025-08-06 17:21:59 +0000

    delete download pdf

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,12 +15,14 @@ export default function Hero() {
         />
       </div>
       <div className="text-center mt-8">
-        <Link href={"https://portfolio.devadh.com"} target="_blank"  className="bg-purple-900 text-white px-4 py-2 rounded  hover:bg-purple-700 transition-colors">
+        <Link
+          href={"https://portfolio.devadh.com"}
+          target="_blank"
+          className="bg-purple-900 text-white px-4 py-2 rounded  hover:bg-purple-700 transition-colors"
+        >
           My Portfolio
         </Link>
-        <a href="/docs/doc.pdf" download>
-  Download PDF
-</a>
+
       </div>
     </div>
   );
